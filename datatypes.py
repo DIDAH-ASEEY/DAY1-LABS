e5fe91bd4710e21742a791009b27821bb3e362e1
DIDAH-ASEEY <aseeyd@gmail.com>
2017-01-18 19:10:34 +0300

    data types lab

---
+++ datatypes.py
@@ -0,0 +1,24 @@
+def data_type(x):
+  if type(x)==str:
+    return len(x)
+    
+  elif type(x)==None:
+    return "no value"
+    
+  elif type(x)==bool:
+    return x
+    
+  elif type(x)==int:
+    if x>100:
+      return "more than 100"
+    elif x<100:
+      return "less than 100"
+    elif x==100:
+      return "equal to 100"
+  
+  elif type(x)==list:
+    if len(x)>=3:
+      return x[2]
+    else:
+      return None
+      
